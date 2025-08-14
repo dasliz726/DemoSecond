98df86b8846386d5470ba1f4500af964e95e2a4f
Admin <Admin@DESKTOP-VANE025>
2025-08-14 20:41:21 +0530

    First push

---
+++ DummyProject/src/test/java/contact_Test/ToCreateContactTest.java
@@ -0,0 +1,9 @@
+package contact_Test;
+
+public class ToCreateContactTest {
+
+	public static void main(String[]args) {
+		System.out.println("Contact Created Successfully");
+	}
+
+}
---
+++ DummyProject/src/test/java/organization_Test/ToCreateOrganizationTest.java
@@ -0,0 +1,10 @@
+package organization_Test;
+
+public class ToCreateOrganizationTest {
+
+	public static void main(String[] args) {
+		System.out.println("Organization Created Successfully");
+
+	}
+
+}
